4efb9325748b166a46569e1e73f2443403874b1d
Alexander A. Prokhorov <prokher@gmail.com>
2019-08-23 16:46:20 +0300

    Spelling.

--- example/settings.py
+++ example/settings.py
@@ -9,8 +9,7 @@
 # the following conditions:
 #
 # The above copyright notice and this permission notice shall be
-# included in all copies or sub
-# ntial portions of the Software.
+# included in all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
