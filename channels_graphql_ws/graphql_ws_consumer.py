5f0a0abb7e91901783169e4a7a5fb9060b80afcc
alexander.kabakov <alexander.kabakov@datadvance.net>
2023-03-29 11:06:00 +0300

    Typo in comment

--- channels_graphql_ws/graphql_ws_consumer.py
+++ channels_graphql_ws/graphql_ws_consumer.py
@@ -1122,7 +1122,7 @@ class GraphqlWsConsumer(ch_websocket.AsyncJsonWebsocketConsumer):
         """Send GraphQL `complete` message to the client.
 
         Args:
-            operation_id: If of the corresponding operation.
+            operation_id: Id of the corresponding operation.
 
         """
         await self.send_json({"type": "complete", "id": operation_id})
